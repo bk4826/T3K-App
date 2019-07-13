8632f0f83dbe8d1c7505a62c06a05b6850a6c22a
T3K <bk4826@outlook.com>
2019-07-13 21:28:20 +0300

    Αλλαγούλες
    Μικροπράματα

--- src/App.js
+++ src/App.js
@@ -286,7 +286,7 @@ export default class App extends Component {
           <form className='todo-create-wrapper' onSubmit={this.saveTodo}>
             <input
               className='todo-create-input'
-              placeholder='Add a todo item'
+              placeholder='Καμιά ιδέα ...'
               name='name'
               ref={el => this.inputElement = el}
               autoComplete='off'
